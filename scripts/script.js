32462969ed814f8019cf66d972b94ae26998c4f3
mikey1201 <159095682+mikey1201@users.noreply.github.com>
2024-07-28 21:06:25 -0400

    changes to searchbar

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
+
     const searchBar = document.getElementById("search-bar");
     const suggestionsContainer = document.getElementById("suggestions");
     const suggestionsArea = document.getElementById("suggestions-box");
@@ -46,14 +47,17 @@ document.addEventListener("DOMContentLoaded", () => {
             })
             //display the suggestions
             if (filteredMovies.length === 0) {
+                searchBar.style.borderRadius = '18px';
                 suggestionsContainer.style.display = 'none';
                 suggestionsArea.style.display = 'none';
             } else { //if there are no matches hide suggestions
+                searchBar.style.borderRadius = '18px 18px 0 0';
                 suggestionsContainer.style.display = 'block';
                 suggestionsArea.style.display = 'block'
             }
         } else {
             //if the search box is empty hide suggestions
+            searchBar.style.borderRadius = '18px';
             suggestionsContainer.style.display = 'none';
             suggestionsArea.style.display = 'none';
         }
