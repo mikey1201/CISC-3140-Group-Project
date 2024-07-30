9f4241ea2002df37bc3639cb0b3fffddb552a580
JosephGarcia98 <garciajoseph998@yahoo.com>
2024-07-30 19:45:23 -0400

    Add files via upload

---
+++ My Table/javascript.js
@@ -0,0 +1,7 @@
+function openBox(){/*dims the screen when movieBox is open*/
+	document.getElementById("overlayDim").style.display = "block";
+}
+
+function closeBox(){/*undims the screen when movieBox is closed*/
+	document.getElementById("overlayDim").style.display = "none";
+}
