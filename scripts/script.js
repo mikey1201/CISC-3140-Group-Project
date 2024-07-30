627012ca44b81046e424915bd02a62ee23ff3964
JosephGarcia98 <garciajoseph998@yahoo.com>
2024-07-30 15:07:05 -0400

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -87,3 +87,10 @@ document.addEventListener("DOMContentLoaded", () => {
     })
 });
 
+function openBox(){/*dims the screen when movieBox is open*/
+	document.getElementById("overlayDim").style.display = "block";
+}
+
+function closeBox(){/*undims the screen when movieBox is closed*/
+	document.getElementById("overlayDim").style.display = "none";
+}
