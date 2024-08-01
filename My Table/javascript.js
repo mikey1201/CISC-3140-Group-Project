b4fb0898e028a66a497a5ee4b77a98deb6d21159
JosephGarcia98 <garciajoseph998@yahoo.com>
2024-08-01 04:26:10 -0400

    Delete My Table directory

--- My Table/javascript.js
+++
@@ -1,7 +0,0 @@
-function openBox(){/*dims the screen when movieBox is open*/
-	document.getElementById("overlayDim").style.display = "block";
-}
-
-function closeBox(){/*undims the screen when movieBox is closed*/
-	document.getElementById("overlayDim").style.display = "none";
-}
